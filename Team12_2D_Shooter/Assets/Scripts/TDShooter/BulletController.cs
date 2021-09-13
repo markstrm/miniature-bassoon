6949d6a1d820b83a80371845f305440f564add64
markstrm <markus.bergstrom@outlook.com>
2021-09-13 16:17:34 +0200

    something

---
+++ Team12_2D_Shooter/Assets/Scripts/TDShooter/BulletController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed = 4f;
+
+    IEnumerator DestroyBulletAfterTime()
+    {
+        yield return new WaitForSeconds(3f);
+        Destroy(gameObject);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Team12_2D_Shooter/Assets/Scripts/TDShooter/TDPlayerController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TDPlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bullet;
+    [SerializeField]
+    private Transform bulletDirection;
+    private TDActions controls;
+    private bool canShoot = true;
+    private Camera main;
+
+    private void Awake()
+    {
+        controls = new TDActions();
+
+        
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+            
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        main = Camera.main;
+        controls.Player.Shoot.performed += _ => PlayerShoot();
+    }
+
+    private void PlayerShoot()
+    {
+        if (!canShoot) return;
+
+        Vector2 mousePosition = controls.Player.MousePosition.ReadValue<Vector2>();
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        GameObject g = Instantiate(bullet, bulletDirection.position, bulletDirection.rotation);
+        g.SetActive(true);
+        StartCoroutine(CanShoot());
+    }
+
+    IEnumerator CanShoot()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(.5f);
+        canShoot = true;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mouseScreenPosition = controls.Player.MousePosition.ReadValue<Vector2>();
+        Vector3 mouseWorldPosition = main.ScreenToWorldPoint(mouseScreenPosition);
+        Vector3 targetDirection = mouseWorldPosition - transform.position;
+        float angle = Mathf.Atan2(targetDirection.y, targetDirection.x);
+        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+    }
+
+
+}
+
+
+
+
